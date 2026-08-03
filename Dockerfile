e6ebde9a659da0d1f06c0a69468f3f8403e4aa31
Anastasiia Salenko <ao.salenko@gmail.com>
2026-08-03 18:33:12 +0300

    change dockerfile

--- Dockerfile
+++ Dockerfile
@@ -11,9 +11,10 @@ USER node
 RUN npm ci
 
 COPY --chown=node:node . .
+RUN npm run build
 
+ENV HOST=0.0.0.0
 ENV PORT=4321
-
 EXPOSE 4321
 
-CMD ["npm", "run", "dev", "--", "--host", "0.0.0.0"]
+CMD ["node", "./dist/server/entry.mjs"]
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
